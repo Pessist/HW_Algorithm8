6f57fd6883cb877f108b47bff4812d5de723f20a
Anton <pessist@rambler.ru>
2021-03-21 12:08:11 +0300

    Add project files.

---
+++ HW_Algorithm8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW_Algorithm8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
